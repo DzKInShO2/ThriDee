7d98eabed761159dbb2907d49249dc07944baa7f
DzKInShO2 <dzkinsho2@protonmail.com>
2025-05-28 16:35:36 +0800

    tambah apamungkin

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,4 +4,9 @@
 	let { children } = $props();
 </script>
 
+<nav class="flex item-align-center outline bg-black text-white gap-x-2 justify-center items-center font-bold h-12">
+    <a class="bg-[#ffff00] text-[#ff0000] p-[10px] rounded-full active:text-black hover:bg-[#ff00ff]" href="/">Home</a>
+    <a class="bg-[#ffff00] text-[#ff0000] p-[10px] rounded-full active:text-black hover:bg-[#ff00ff]" href="/about">About</a>
+</nav>
+
 {@render children()}
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     let total = 5;
 </script>
-<h1>Welcome to SvelteKit</h1>
+<h1>Welcome to About</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 <p>{total}</p>
