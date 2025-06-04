56e438724e8622a04d4b9d187802c73cfa6e9bc7
DzKInShO2 <dzkinsho2@protonmail.com>
2025-06-04 14:40:33 +0800

    Test

--- src/routes/about/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-    let total = 5;
-</script>
-<h1>Welcome to About</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<p>{total}</p>
