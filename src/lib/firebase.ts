bf058a4b4079ec2106ce8bd5d9bc6a924ce1c8cb
DzKInShO2 <dzkinsho2@protonmail.com>
2025-06-04 14:18:27 +0800

    Test

---
+++ src/lib/firebase.ts
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, type FirebaseApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBxyM5mJONdLxPYyU9D4fsf_SuU9Zro-YU",
+    authDomain: "thridee-1d812.firebaseapp.com",
+    projectId: "thridee-1d812",
+    storageBucket: "thridee-1d812.firebasestorage.app",
+    messagingSenderId: "248817102061",
+    appId: "1:248817102061:web:9c625dda83f49615cd30e4"
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,9 +4,25 @@
 	let { children } = $props();
 </script>
 
-<nav class="flex item-align-center outline bg-black text-white gap-x-2 justify-center items-center font-bold h-12">
-    <a class="bg-[#ffff00] text-[#ff0000] p-[10px] rounded-full active:text-black hover:bg-[#ff00ff]" href="/">Home</a>
-    <a class="bg-[#ffff00] text-[#ff0000] p-[10px] rounded-full active:text-black hover:bg-[#ff00ff]" href="/about">About</a>
+<nav class="flex flex-row justify-between items-center h-15 p-4 shadow-xs">
+    <div>
+        <img src="/logo.png" class="w-24 h-24">
+    </div>
+    <div>
+        <form class="flex flex-row justify-between shadow-sm w-150 rounded-xl overflow-clip">
+            <input type="text" name="s" id="searchbar" placeholder="Search your 3D assets..." class="border-0 flex-9">
+            <button class="bg-[#FFA808] active:bg-[#F75B00] w-10 flex flex-col items-center justify-center">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4 mr-1.5">
+                    <path fill-rule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clip-rule="evenodd" />
+                </svg>
+            </button>
+        </form>
+    </div>
+    <div>
+        <button class="bg-[#FFA808] active:bg-[#F75B00] p-2 rounded-xl">
+            Log-In
+        </button>
+    </div>
 </nav>
 
 {@render children()}
