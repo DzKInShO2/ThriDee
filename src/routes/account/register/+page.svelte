7fcbde17d079dfd9dc0f675a9f5acdd51f7d24f8
DzKInShO2 <dzkinsho2@protonmail.com>
2025-06-05 13:33:04 +0800

    Fix width

--- src/routes/account/register/+page.svelte
+++ src/routes/account/register/+page.svelte
@@ -9,18 +9,18 @@ $: if ($user) {
 
 <div class="flex flex-col items-center gap-10 mt-20 lg:mt-50">
     <h1 class="text-3xl font-medium">Register</h1>
-    <form method="POST" class="flex flex-col gap-5 shadow-2xl rounded-4xl bg-[#FAFAFA] p-10 w-100 lg:w-120" autocomplete="nope">
+    <form method="POST" class="flex flex-col gap-5 shadow-2xl rounded-4xl bg-[#FAFAFA] p-10 w-90 lg:w-120" autocomplete="nope">
         <div class="flex flex-col gap-2">
             <label class="font-light">Email</label>
-            <input type="email" name="email" placeholder="Your email..." class="border-0 shadow-md rounded-lg w-80 lg:w-100" required>
+            <input type="email" name="email" placeholder="Your email..." class="border-0 shadow-md rounded-lg w-70 lg:w-100" required>
         </div>
         <div class="flex flex-col gap-2">
             <label class="font-light">New Password</label>
-            <input type="password" name="password" placeholder="Your password..." class="border-0 shadow-md rounded-lg w-80 lg:w-100" required>
+            <input type="password" name="password" placeholder="Your password..." class="border-0 shadow-md rounded-lg w-70 lg:w-100" required>
         </div>
         <div class="flex flex-col gap-2">
             <label class="font-light">Verify Password</label>
-            <input type="password" name="password" placeholder="Your password..." class="border-0 shadow-md rounded-lg w-80 lg:w-100" required>
+            <input type="password" name="password" placeholder="Your password..." class="border-0 shadow-md rounded-lg w-70 lg:w-100" required>
         </div>
         <button class="transition-all bg-[#FFA808] p-2 rounded-xl cursor-pointer hover:shadow-lg hover:p-3 active:bg-[#F75B00] mt-5">
             Register
