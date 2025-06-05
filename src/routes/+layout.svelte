d5c2d6e3849304a5c1c732203f33d3b9f03770ce
DzKInShO2 <dzkinsho2@protonmail.com>
2025-06-05 13:25:16 +0800

    tambah sidebar mobile

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,56 @@
 <script lang="ts">
 	import '../app.css';
+
+    import { auth } from "$lib/firebase";
+    import { user } from "$lib/stores/authStore";
+    import { signOut } from "firebase/auth";
 	
 	let { children } = $props();
+
+    let sidebarPosition = $state("-left-60");
+    function toggleSidebar()
+    {
+        if (sidebarPosition === "-left-60") {
+            sidebarPosition = "-left-0";
+        } else {
+            sidebarPosition = "-left-60";
+        }
+    }
+
+    let search = "";
+    function searchAssets() 
+    {
+    }
 </script>
 
-<nav class="flex flex-row justify-between items-center h-15 p-4 shadow-xs gap-5">
+{#snippet loginButton(visibility)}
+    <a href="/account/login"><button class="transition-all cursor-pointer bg-[#ffa808] p-2 rounded-xl hover:shadow-lg hover:p-3 active:bg-[#f75b00] {visibility}">
+        Log-In
+    </button></a>
+{/snippet}
+
+{#snippet logoutButton(visibility)}
+    <button class="transition-all cursor-pointer bg-[#ffa808] p-2 rounded-xl hover:shadow-lg hover:p-3 active:bg-[#f75b00] {visibility}" on:click={() => signOut(auth)}>
+        Log-Out
+    </button>
+{/snippet}
+
+<nav class="flex flex-row justify-between items-center h-15 p-4 shadow-xs gap-5 sticky z-40">
     <div>
         <a href="/">
             <img src="/logo.png" class="w-24 h-24 hidden lg:block">
         </a>
-        <h1 class="block lg:hidden">Side</h1>
+        <h1 class="transition-all block lg:hidden active:text-[#FFA808]" on:click={toggleSidebar}>
+            {#if sidebarPosition === "-left-60"}
+                <i class="fa-solid fa-bars"></i>
+            {:else}
+                <i class="fa-solid fa-x"></i>
+            {/if}
+        </h1>
     </div>
     <div>
-        <form class="flex flex-row justify-between shadow-sm rounded-xl overflow-clip w-120 md:w-160">
-            <input type="text" name="s" id="searchbar" placeholder="Search your 3D assets..." class="border-0 w-120 md:w-160">
+        <form on:submit|preventDefault={searchAssets} class="flex flex-row justify-between shadow-sm rounded-xl overflow-clip w-80 md:w-160">
+            <input type="text" name="s" id="searchbar" placeholder="Search your 3D assets..." class="border-0 w-120 md:w-160 flex-9">
             <button class="bg-[#FFA808] cursor-pointer  active:bg-[#F75B00] w-10 flex flex-col items-center justify-center">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4 mr-1.5">
                     <path fill-rule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clip-rule="evenodd" />
@@ -22,11 +59,26 @@
         </form>
     </div>
     <div>
-        <a href="/login"><button class="transition-all cursor-pointer bg-[#FFA808] p-2 rounded-xl hidden hover:shadow-lg hover:p-3 active:bg-[#F75B00] lg:block">
-            Log-In
-        </button></a>
+        {#if $user}
+            {@render logoutButton("hidden lg:block")}
+        {:else}
+            {@render loginButton("hidden lg:block")}
+        {/if}
     </div>
 </nav>
 
+<div class="transition-all ease-in-out duration-500 fixed bg-white border-r-2 border-[#F7F7F7] shadow-2xl h-screen w-60 {sidebarPosition} p-4 z-40">
+    <div class="flex flex-col justify-between items-center gap-3">
+        {#if $user}
+            <h1>{$user.displayName}</h1>
+            {@render logoutButton("")}
+        {:else}
+            {@render loginButton("")}
+        {/if}
+    </div>
+</div>
+
+<div class="z-10">
 {@render children()}
+</div>
 
---
+++ src/routes/account/register/+page.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+import { goto } from "$app/navigation";
+import { user } from "$lib/stores/authStore";
+
+$: if ($user) {
+    goto("/");
+}
+</script>
+
+<div class="flex flex-col items-center gap-10 mt-20 lg:mt-50">
+    <h1 class="text-3xl font-medium">Register</h1>
+    <form method="POST" class="flex flex-col gap-5 shadow-2xl rounded-4xl bg-[#FAFAFA] p-10 w-100 lg:w-120" autocomplete="nope">
+        <div class="flex flex-col gap-2">
+            <label class="font-light">Email</label>
+            <input type="email" name="email" placeholder="Your email..." class="border-0 shadow-md rounded-lg w-80 lg:w-100" required>
+        </div>
+        <div class="flex flex-col gap-2">
+            <label class="font-light">New Password</label>
+            <input type="password" name="password" placeholder="Your password..." class="border-0 shadow-md rounded-lg w-80 lg:w-100" required>
+        </div>
+        <div class="flex flex-col gap-2">
+            <label class="font-light">Verify Password</label>
+            <input type="password" name="password" placeholder="Your password..." class="border-0 shadow-md rounded-lg w-80 lg:w-100" required>
+        </div>
+        <button class="transition-all bg-[#FFA808] p-2 rounded-xl cursor-pointer hover:shadow-lg hover:p-3 active:bg-[#F75B00] mt-5">
+            Register
+        </button>
+
+        <p class="block m-auto">Aleady have an account? <a href="/account/login" class="text-[#FFA808] hover:text-[#F75B00] cursor-pointer">login here</a>.</p>
+    </form>
+    <button class="cursor-pointer rounded-xl flex items-center gap-5 justify-center shadow-md h-12 w-90 lg:w-110 hover:brightness-60 active:brightness-110 bg-[#FFFFFF]">
+        <img class="w-6 h-6" src="https://www.svgrepo.com/show/475656/google-color.svg" loading="lazy" alt="google logo">
+        Register With Google
+    </button>
+</div>
